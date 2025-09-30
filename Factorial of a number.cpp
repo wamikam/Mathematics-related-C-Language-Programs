95dc92d51098aa934a63bef6ef6dbf345d1e017b
wamikam <74076827+wamikam@users.noreply.github.com>
2025-09-30 14:38:44 +0100

    Add files via upload

---
+++ Factorial of a number.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long factorial = 1; // Use unsigned long long for larger factorials
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    // Factorial is not defined for negative numbers
+    if (n < 0) {
+        printf("Error! Factorial of a negative number doesn't exist.\n");
+    } else {
+        // Loop to calculate factorial
+        for (i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        printf("Factorial of %d = %llu\n", n, factorial);
+    }
+
+    return 0;
+}
